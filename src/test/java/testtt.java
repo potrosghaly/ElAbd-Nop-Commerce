f75c1c70a1a6c7d8e3504d74f5c92f819bd97a30
Admin <potrossghaly@gamil.com>
2025-10-22 16:26:57 +0300

    Stock Test

--- src/test/java/testtt.java
+++ src/test/java/testtt.java
@@ -2,17 +2,13 @@ import base.BaseTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.List;
-
 import static reader.ReadDataFromJson.dataModel;
 
 public class testtt extends BaseTests {
 
 
     @Test
-    public void testStock() throws Exception {
+    public void testStockAfterDeliverdCashOnDelivery() throws Exception {
 
         var login = homePage.clickLoginIcon();
         login.writePhoneNumber(dataModel().phone);
@@ -30,14 +26,20 @@ public class testtt extends BaseTests {
         product.checkStock();
         int oldStock = product.getStock();
         int oldReserved = product.getReserved();
-        System.out.println(oldStock);
-        System.out.println(oldReserved);
+
         String productName = product.getProductName();
         // save old tab name
         String productTab = saveTab();
         // open new tab and go to home page
         openNewTab();
         goHomePage();
+
+        login = homePage.clickLoginIcon();
+        login.writePhoneNumber(dataModel().phone);
+        login.clickSendOtpButton();
+        login.writePassword(dataModel().password);
+        homePage = login.clickLogin();
+
         homePage.searchByProductName(productName);
         // create order
         homePage.addProductToCart();
@@ -55,44 +57,63 @@ public class testtt extends BaseTests {
         int newStock = product.getStock();
         int newReserved = product.getReserved();
 
-        System.out.println(oldStock + "-->" + newStock);
-        System.out.println(oldReserved + "-->" + newReserved);
+        System.out.println("old StocK = " + oldStock + "-->" + "New StocK = " +newStock);
+        System.out.println("old Reserved = " +oldReserved + "-->" +"new Reserved = " +  newReserved);
+
+        openNewTab();
+        goHomePage();
+
+        admin = homePage.openAdmin();
+        var salePage = admin.openSalesPage();
+        salePage.resestFilter();
+        salePage.clickSearchButton();
+        var productPage = salePage.openFirstOrder();
+        Assert.assertTrue(productPage.pendingStatusIsAppear());
+        productPage.clickPreparingButton();
+        productPage.clickOnWayButton();
+        productPage.clickDeliveredButton();
+
+        // close new tab
+        closeTab();
+        // back to product tab
+        backToTab(productTab);
+        refreshPage();
+        product.getStocks();
+        newStock = product.getStock();
+        newReserved = product.getReserved();
+        System.out.println("after order delivered");
+        System.out.println("old StocK = " + oldStock + "-->" + "New StocK = " +newStock);
+        System.out.println("old Reserved = " +oldReserved + "-->" +"new Reserved = " +  newReserved);
 
         sleepPerSeconds(3);
     }
 
 
-    @Test
-    public void getMerchantNumberLoop() throws FileNotFoundException, InterruptedException, FileNotFoundException {
+//    @Test
+//    public void getMerchantNumberLoop() throws FileNotFoundException, InterruptedException, FileNotFoundException {
+//
+//        var login = homePage.clickLoginIcon();
+//        sleepPerSeconds(1);
+//        login.writePhoneNumber(dataModel().liveData.livephone);
+//        login.clickSendOtpButton();
+//        login.writePassword(dataModel().liveData.livepassword);
+//        var homePage = login.clickLogin();
+//        int index = 0;
+//
+//        while (index < ID.size()) {
+//            var test = homePage.goToTestPage();
+//            test.openOrderDetailsPage(ID.get(index));
+//            System.out.println(ID.get(index)+ " --> " +test.getMerchantNumber());
+//            index = index + 1  ;
+//
+//        }
+//
+//
+//    }
+//
+//    List<Integer> ID = Arrays.asList(1414986);
 
-        var login = homePage.clickLoginIcon();
-        sleepPerSeconds(1);
-        login.writePhoneNumber(dataModel().liveData.livephone);
-        login.clickSendOtpButton();
-        login.writePassword(dataModel().liveData.livepassword);
-        var homePage = login.clickLogin();
-        int index = 0;
 
-        while (index < ID.size()) {
-            var test = homePage.goToTestPage();
-            test.openOrderDetailsPage(ID.get(index));
-            System.out.println(ID.get(index)+ " --> " +test.getMerchantNumber());
-            index = index + 1  ;
-
-        }
-
-
-    }
-
-    List<Integer> ID = Arrays.asList(
-            1414986
-    );
-
-    @Test
-    public void getOrderIDTest()
-    {
-
-    }
 
 
 }
